2506941c6cfc65687fca2ff52cc4866a201557bc
bandoss81 <bandoss81@gmail.com>
2022-06-27 13:34:37 +0300

    homework number 1

--- PracticalTask001/Program.cs
+++ PracticalTask001/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2
+
+
+int num1, num2;
+
+Console.Write("Input a first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a first number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if(num1 < num2)
+{
+    Console.WriteLine("Max = " + num2);
+}
+else
+{
+    Console.WriteLine("Max = " + num1);
+}
