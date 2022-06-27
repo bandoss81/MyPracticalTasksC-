fac747ecd0b7ae1bae63bccabf5d5888fe72411d
bandoss81 <bandoss81@gmail.com>
2022-06-27 13:36:02 +0300

    homework number 2

--- PracticalTask001/Program.cs
+++ PracticalTask001/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 2
 
-
+/*
 int num1, num2;
 
 Console.Write("Input a first number: ");
@@ -17,3 +17,27 @@ else
 {
     Console.WriteLine("Max = " + num1);
 }
+*/
+
+// Задача 4
+
+int num1, num2, num3, max;
+
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+max = num1;
+if(num1 < num2)
+{
+    max = num2;
+    if(num2 < num3)
+    {
+        max = num3;
+    }
+}
+
+Console.WriteLine("Максималиное число: " + max);
