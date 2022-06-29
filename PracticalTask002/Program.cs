56d6597fafba4a9727bd6dc9f1dc0bc274db0f52
bandoss81 <bandoss81@gmail.com>
2022-06-29 18:35:07 +0300

    Homework. Task 10

--- PracticalTask002/Program.cs
+++ PracticalTask002/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10
+
+void CutNumberVoid()
+{
+    int num = new Random().Next(100,1000);
+    Console.WriteLine("Текущее случайное число: " + num);
+
+    int sot = num / 100;
+    int ed = num % 10;
+
+    int result = (num - sot * 100 - ed) / 10;
+    Console.WriteLine("Вторая цифра этого числа: " + result);
+}
+
+CutNumberVoid();
