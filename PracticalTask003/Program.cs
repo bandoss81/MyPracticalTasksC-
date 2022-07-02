aedbbfb4c84fd0dd06a27c53f5d9efbd93b9ecbd
bandoss81 <bandoss81@gmail.com>
2022-07-02 15:27:32 +0300

    Homework. Task 23

--- PracticalTask003/Program.cs
+++ PracticalTask003/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 19
-
+/*
 Console.Write("Введите пятизначное число: ");
 string num = Console.ReadLine();
 
@@ -17,10 +17,28 @@ if (num.Length == 5)
   PalindromeNumber(num);
 }
 else Console.WriteLine("Введи правильное число");
+*/
 
+// Задача 23
 
+Console.Write("Введите число (N): ");
+int number = Convert.ToInt32(Console.ReadLine());
 
+void Cube(int number)
+{
+    int cube;
+    int count = 1;
+    while(count <= number)
+    {
+        cube = count * count * count;
+        
+        Console.WriteLine(cube);
+        count++;
+    }
+}
 
+Console.WriteLine("Таблица кубов чисел от 1 до N: ");
+Cube(number);
 
 
 
