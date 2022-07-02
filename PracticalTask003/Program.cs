7a1dd69b861932435abbc4a0086efce3cc205b80
bandoss81 <bandoss81@gmail.com>
2022-07-02 15:07:31 +0300

    Homework. Task 19

--- PracticalTask003/Program.cs
+++ PracticalTask003/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Задача 19
+
+Console.Write("Введите пятизначное число: ");
+string num = Console.ReadLine();
+
+void PalindromeNumber(string num)
+{
+  if (num[0] == num[4] && num[1] == num[3])
+  {
+    Console.WriteLine("Ваше число: - палиндром.");
+  }
+  else Console.WriteLine("Ваше число: - НЕ палиндром.");
+}
+
+if (num.Length == 5)
+{
+  PalindromeNumber(num);
+}
+else Console.WriteLine("Введи правильное число");
+
+
+
+
+
+
+
+
+
