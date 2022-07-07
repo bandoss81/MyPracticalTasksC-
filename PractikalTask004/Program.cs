53ce4939b4e5c3ad1c41db80fc731c7b4d5e68fa
bandoss81 <bandoss81@gmail.com>
2022-07-07 17:09:04 +0300

    Homework. Task 27

--- PractikalTask004/Program.cs
+++ PractikalTask004/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+int SumNumber(int num)
+{
+    
+  int counter = Convert.ToString(num).Length;
+  int ten = 0;
+  int result = 0;
+
+  for (int i = 0; i < counter; i++)
+  {
+    ten = num - num % 10;
+    result = result + (num - ten);
+    num = num / 10;
+  }
+   return result;
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sumNumber = SumNumber(num);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
+
+
+
+
+
