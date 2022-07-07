36b3af50c1d9cad27147de8007f856774748e699
bandoss81 <bandoss81@gmail.com>
2022-07-07 17:12:35 +0300

    Homework. Task 29

--- PractikalTask004/Program.cs
+++ PractikalTask004/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
 
-
+/*
 int SumNumber(int num)
 {
     
@@ -21,10 +21,38 @@ Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int sumNumber = SumNumber(num);
 Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+*/
 
 
 
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
 
+Console.Write("Какое количество рандомных цифр вывести на экран: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+void Main()
+{
+  
+  int[] array = new int[m];
+  Random myRandom = new Random();
+ 
+  
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = myRandom.Next(0, 20);
+    Console.Write("{0}", array[i]);
+    if (i < (array.Length - 1))
+    {
+      Console.Write(", ");
+    }
+  }
+  
+}
+
+Console.Write("[");
+Main();
+Console.Write("]");
 
 
 
