47bc31ef3cdfab58fa1ce5b031af9417db16bb42
bandoss81 <bandoss81@gmail.com>
2022-07-17 17:43:59 +0300

    Homework. Task52

--- PracticalTask007/Program.cs
+++ PracticalTask007/Program.cs
@@ -84,20 +84,20 @@ void ShowArrayInt(int[,] arrayInt)
     }
 }
 
-ShowArrayInt(arrayInt);
+//ShowArrayInt(arrayInt);
 
-Console.Write("Введите координаты позиции элемента, разделенных запятой: ");
+// Console.Write("Введите координаты позиции элемента, разделенных запятой: ");
 
-string? positionElement = Console.ReadLine();
-positionElement = RemovingSpaces(positionElement);
-int[] position = ParserString(positionElement);
+// string? positionElement = Console.ReadLine();
+// positionElement = RemovingSpaces(positionElement);
+// int[] position = ParserString(positionElement);
 
-if (position[0] <= m && position[1] <= n && position[0] >= 0 && position[1] >= 0)
-{
-    int result = arrayInt[position[0] - 1, position[1] - 1];
-    Console.Write($"Значение элемента: {result}");
-}
-else Console.Write($"такого элемента в массиве нет.");
+// if (position[0] <= m && position[1] <= n && position[0] >= 0 && position[1] >= 0)
+// {
+//     int result = arrayInt[position[0] - 1, position[1] - 1];
+//     Console.Write($"Значение элемента: {result}");
+// }
+// else Console.Write($"такого элемента в массиве нет.");
 
 int[] ParserString(string input)
 {
@@ -140,4 +140,26 @@ string RemovingSpaces(string input)
         }
     }
     return output;
+}
+
+// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов
+// в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+ShowArrayInt(arrayInt);
+
+Console.Write($"Cреднее арифметическое:\n");
+for (int i = 0; i < n; i++)
+{
+    double arithmeticMean = 0;
+    for (int j = 0; j < m; j++)
+    {
+        arithmeticMean += arrayInt[j, i];
+    }
+    arithmeticMean = Math.Round(arithmeticMean / m, 1);
+    Console.WriteLine($"столбца {i + 1}: {arithmeticMean}");
 }
