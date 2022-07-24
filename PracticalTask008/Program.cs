8f21b5b49f3a59f09a7b71357f3b1bc7eb9d5533
bandoss81 <bandoss81@gmail.com>
2022-07-24 17:11:10 +0300

    Homework. Task54

--- PracticalTask008/Program.cs
+++ PracticalTask008/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
+// каждой строки двумерного массива.
+
+
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows,columns];
+
+    for(int i = 0; i < rows; i++)
+       for(int j = 0; j < columns; j++)
+           array[i,j] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;        
+}
+
+void Show2dArray (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();    
+    }
+}
+
+void OrderArrayLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Минимальное значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Максимальное значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
+int[,] array = CreateRandom2dArray(m, n, min, max);
+Show2dArray(array);
+Console.WriteLine($"\nОтсортированный массив: ");
+OrderArrayLines(array);
+Show2dArray(array);
+
+
+
