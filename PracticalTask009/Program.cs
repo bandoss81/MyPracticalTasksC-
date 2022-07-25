353803ca76e5c2389240e5d6096461397f40d907
bandoss81 <bandoss81@gmail.com>
2022-07-25 19:50:35 +0300

    Homework Task 64

--- PracticalTask009/Program.cs
+++ PracticalTask009/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным
+// методом.
+// N = 4532 -> 4
+
+Console.Clear();
+
+int n = InputNumbers("Введите n: ");
+Recur(n);
+Console.WriteLine($"Количество цифр в числе N = {Recur(n)}");
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int Recur(int n)
+{
+    return n < 10 ? 1 : 1 + Recur(n / 10);
+}
+
+
+
