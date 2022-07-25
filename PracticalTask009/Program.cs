c371cae0dc053835ee0293e8995a41ed136f1fa7
bandoss81 <bandoss81@gmail.com>
2022-07-25 19:52:07 +0300

    Homework Task 66

--- PracticalTask009/Program.cs
+++ PracticalTask009/Program.cs
@@ -4,9 +4,9 @@
 
 Console.Clear();
 
-int n = InputNumbers("Введите n: ");
-Recur(n);
-Console.WriteLine($"Количество цифр в числе N = {Recur(n)}");
+// int n = InputNumbers("Введите n: ");
+// Recur(n);
+// Console.WriteLine($"Количество цифр в числе N = {Recur(n)}");
 
 int InputNumbers(string input)
 {
@@ -20,5 +20,33 @@ int Recur(int n)
     return n < 10 ? 1 : 1 + Recur(n / 10);
 }
 
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в
+// промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int count = m;
+PrintSum(m, n, count = 0);
+
+if (m > n)
+{
+    m = n;
+    n = count;
+}
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов = {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+}
 
 
